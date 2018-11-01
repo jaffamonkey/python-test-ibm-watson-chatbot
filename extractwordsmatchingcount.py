c60014b01898f3ce3cbf731e46b37f63e09f80b5
Paul Littlebury <paullittlebury@gmail.com>
2018-11-01 13:46:55 +0100

    Update extractwordsmatchingcount.py

--- extractwordsmatchingcount.py
+++ extractwordsmatchingcount.py
@@ -4,28 +4,27 @@
 import time
 import os
 
-# Send messages, using the message files stored "input" directory
+class ExtractExamplesBasedOnWordCount:
 
-
-class ExtractWordsMatchingCount:
-
-    def extractWordsMatchingCount(count):
+    def extractExamplesBasedOnWordCount(numberWords):
         space_id = os.environ.get('SPACE_ID')
         directory = './input/' + space_id + '/'
 
         # Loop round each filename in directory
         for filename in os.listdir(directory):
             space_count = 0
-            f2 = open(directory + count + '.txt', 'a+') 
             intent = os.path.splitext(filename)[0]
-            lines = filename.readlines()
-            for line in lines:
-                space_count = line.count('')
-                if space_count == count - 1:
-                    f.write(line + ',' + intent + ',' + space_id)
-                    f.close()
+            with open(directory + filename, 'r') as file:
+                for line in file:  
+                    print(line)
+                    f = open(directory + numberWords + '.txt', 'a+') 
+                    space_count = line.count(' ')
+                    print(space_count)
+                    if space_count == numberWords-1:
+                        f.write(line)
+                        f.close()
 
 # if __name__== "__main__":
 #   main()s
 
-ExtractSingleWordExamples.extractWordsMatchingCount(2)
+ExtractExamplesBasedOnWordCount(1)
